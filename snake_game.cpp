dbf17011129387503abcd4f28c105dab2bd578bb
Quan Nguyen <141884431+tuandechai11@users.noreply.github.com>
2024-06-04 10:00:27 +0700

    Can take character input now!

--- snake_game.cpp
+++ snake_game.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <time.h>
+#include <windows.h>
 
 template <typename T>
 void printArray(const T& arr){
@@ -10,8 +12,43 @@ for(int i = 0; i<=7;i++){
 std::cout<<"\n";
 }
 
+void CharacterInput(char &a)
+{
+    bool end=false;
+    clock_t endWait;
+    bool noInput=true;
+    endWait=clock()+1;
+    while (clock()<=endWait && noInput)
+    {
+        if (GetAsyncKeyState('A'))
+        {
+            end=true;
+            noInput=false;
+            a = 'A';
+        }
+        if (GetAsyncKeyState('S'))
+        {
+            end=true;
+            noInput=false;
+            a = 'S';
+        }
+        if (GetAsyncKeyState('D'))
+        {
+            end=true;
+            noInput=false;
+            a = 'D';
+        }
+        if (GetAsyncKeyState('W'))
+        {
+            end=true;
+            noInput=false;
+            a = 'W';
+        }
+    }
+}
 
 int main(){
+    char movement;
 int myArray1[8] = {0,0,0,0,0,0,0,0};
 int myArray2[8] = {0,0,0,0,0,0,0,0};
 int myArray3[8] = {0,0,0,0,0,0,0,0};
@@ -30,7 +67,8 @@ while (true) {
         printArray(myArray6);
         printArray(myArray7);
         printArray(myArray8);
-        std::cout<<"\n";
+        CharacterInput(movement);
+        std::cout<<movement<<"\n";
         std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // Delay for 1 second
 }
 return 0;
